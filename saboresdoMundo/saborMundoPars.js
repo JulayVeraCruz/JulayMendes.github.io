ed054bb51692ce2fcff838c6c29729a06448ebca
JulayM <jm513@hotmail.com>
2018-10-30 19:55:20 +0000

    adding my files to my repository

---
+++ saboresdoMundo/saborMundoPars.js
@@ -0,0 +1,48 @@
+﻿//discorPars.js****************************************************************************
+var docXML;
+//Através de um ficheiro XML no servidor, e de um objeto XMLhttpRequest é produzida a variávl docXML, capaz de ser
+//processada em javascript no cliente. Em simultâneo, será iniciada a função principal inic().
+
+function inicXML(fichXML) {
+    //Criação de um objecto XMLHTTPREQUEST
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            docXML = xmlhttp.responseXML; inic();
+        }
+    }
+    xmlhttp.open("GET", fichXML, true);
+    xmlhttp.send();
+
+}
+
+//EXTRAÇÂO DE DADOS
+//titulo da aplicação
+function temaR() {
+    var rec = docXML.getElementsByTagName("introducao")[0];
+    return rec.getAttribute("tema");
+}
+//mostra imagem de esquerda
+function imgIntro() {
+    var rec = docXML.getElementsByTagName("introducao")[0];
+    return "imagem/" + rec.getAttribute("logotipo");
+
+}
+
+//IMAGEM central
+function imgParag() {
+    var rec = docXML.getElementsByTagName("introducao")[0];
+    return "imagem/" + rec.getAttribute("imgIntroducao");
+}
+
+function  mostraIntro(){
+    var rec = docXML.getElementsByTagName("introducao")[0];
+    return rec.childNodes[0].nodeValue;
+}
+function numReceitas() {
+    return docXML.getElementsByTagName("receitas").length;
+}
+function mostraReceitas(iPais) {
+    var rec = docXML.getElementsByTagName("receitas")[iPais];
+    return rec.getAttribute("pais");
+}
---
+++ saboresdoMundo/saborMundoProcs.js
@@ -0,0 +1,42 @@
+﻿//discotProcs.js
+//************************************************
+
+//OUTSPUTS
+var interf = {}
+function inic() {
+    obterInterface();
+    apresentar();
+   
+}
+//Adquire e publica  referências para interface do sistema
+function obterInterface() {
+    interf.celEsquerda = document.getElementById("celEsquerda");
+    interf.titulo = document.getElementById("titulo");
+    interf.scroll = document.getElementById("scroll");
+    interf.celDireita= document.getElementById("celDireita");
+    interf.intro = document.getElementById("intro");
+
+}
+function apresentar() {
+   
+ //  interf.celEsquerda.innerHTML = temaR();
+    interf.titulo.innerHTML = '<marquee>' + temaR() + '</marquee>';
+    interf.celEsquerda.innerHTML = '<center>' + '<br><img src="' + imgIntro() + '" />' + '</center>';
+    interf.celDireita.innerHTML ='<center class="texto">'+mostraIntro();
+    interf.celDireita.innerHTML += '<br><img src="' + imgParag() + '" />' + '</center>';
+    interf.celEsquerda.innerHTML = CodApresPaisReceita(iReceita);
+
+
+    }
+function CodApresPaisReceita(iReceita){
+    var nrec = numReceitas(), cod ='';
+    cod+= '<div>'
+    for (var i = 0; i < nrec; i++) {
+        '</div>';
+        cod+= '<button>' + mostraReceitas(i) + '</button>';
+       
+    }
+    
+    return cod;
+}
+
